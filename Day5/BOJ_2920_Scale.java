cc5d8d13d58ac599dd409f536c6d8f53dbf7674e
noel <68936@naver.com>
2020-11-08 23:08:48 +0900

    decending 구현중(아직 미구현)

--- Day5/BOJ_2920_Scale.java
+++ Day5/BOJ_2920_Scale.java
@@ -5,34 +5,41 @@ package com.example.Day5;
 //1  1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed 이다.
 //1 연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을 작성하시오.
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BOJ_2920_Scale {
 
     public static void main(String[] args)  throws IOException {
 
-        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
 
         int preNum = 0;
+        int preNum2 = 9;
         int InputNum;
-        int cnt = 0;
+        int ascendingCNT = 0;
+        int decendingCNT = 0;
         for(int i=0;i<9;i++) {
-            InputNum = Integer.parseInt(buff.readLine());
+            InputNum = sc.nextInt();
 
-            if(InputNum == preNum+1) {
+            if(InputNum == ++preNum) {
                 preNum = InputNum;
-                cnt++;
+                ascendingCNT++;
+            }
+            else if(InputNum == --preNum2) {
+                preNum2 = InputNum;
+                decendingCNT++;
             }
 
         }
-
-        if(cnt == 9)
-        {
-
+        if(ascendingCNT == 9) {
             System.out.println("ascending");
         }
+        else if(decendingCNT == 9) {
+            System.out.println("decending");
+        }
+
+        sc.close();
 
 
 
