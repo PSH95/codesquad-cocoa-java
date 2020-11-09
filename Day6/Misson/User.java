c21d4b30ebc1b259877f8c9183ded602552900c0
noel <68936@naver.com>
2020-11-10 04:02:18 +0900

    유저정보 클래스 추가

---
+++ Day6/Misson/User.java
@@ -0,0 +1,23 @@
+package com.example.Day6.Misson;
+
+import java.util.LinkedList;
+
+public class User {
+
+    private LinkedList<String > NAME = new LinkedList<String>();
+    private LinkedList<String > PASSWORD = new LinkedList<String>();
+
+
+    public boolean checkName(String NAME){
+        return this.NAME.contains(NAME); //0 반환값: 참/거짓
+    }
+
+    public void setName(String NAME){
+        this.NAME.add(NAME);
+    }
+    public void setPassword(String PASSWORD){
+        this.NAME.add(PASSWORD);
+    }
+
+
+}
