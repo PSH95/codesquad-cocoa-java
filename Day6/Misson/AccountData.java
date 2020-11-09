70c56985fcccacae8c797411859d2f8b0e5ebf92
noel <68936@naver.com>
2020-11-10 05:06:15 +0900

    LinkedList 활용, 로그인,삽입,삭제,출력 구현

--- Day6/Misson/AccountData.java
+++ Day6/Misson/AccountData.java
@@ -1,4 +1,39 @@
 package com.example.Day6.Misson;
 
+import java.util.LinkedList;
+
 public class AccountData {
+
+    //0 계정마다 구분할 방법 아직 못찾음
+
+    private LinkedList<String > DATE = new LinkedList<String>();
+    private LinkedList<String > CONTENTS = new LinkedList<String>();
+    private LinkedList<Integer > INCOME = new LinkedList<Integer>();
+    private LinkedList<Integer > SPENDING = new LinkedList<Integer>();
+
+    public void addDATE(String DATE){
+        this.DATE.add(DATE);
+    }
+    public void addCONTENTS(String CONTENTS){
+        this.CONTENTS.add(CONTENTS);
+    }
+    public void addINCOME(Integer INCOME){
+        this.INCOME.add(INCOME);
+    }
+    public void addSPENDING(Integer SPENDING){
+        this.SPENDING.add(SPENDING);
+    }
+
+    public void accountLogPrint() {
+        for(int i=0;i<this.DATE.size();i++){
+            System.out.println("["+i+"번] 날짜: "+this.DATE.get(i) + "/ 적요: "+this.CONTENTS.get(i)+ " / 수입: "+this.INCOME.get(i)+ " / 지출: "+this.SPENDING.get(i));
+        }
+    }
+    public void deleteAccountLog(int index) {
+        this.DATE.remove(index);
+        this.CONTENTS.remove(index);
+        this.INCOME.remove(index);
+        this.SPENDING.remove(index);
+    }
+
 }
--- Day6/Misson/AccountMain.java
+++ Day6/Misson/AccountMain.java
@@ -1,4 +1,133 @@
 package com.example.Day6.Misson;
 
+
+// TODO     필수 요구사항
+// TODO     간단한 가계부를 구현한다.
+// TODO     키보드를 통해 데이터 입력을 받고 화면에 내용을 출력한다.
+
+// TODO     사용자 등록: 사용자 이름 및 비밀번호를 입력받는다.
+// TODO     [입력]: 날짜, 적요, 수입, 지출을 입력받는다.
+// TODO     [삭제]: 특정 순번의 데이터를 삭제한다.
+// TODO     [수정]: 특정 순번의 데이터를 수정할 수 있다.
+// TODO     [출력]: 해당 월의 지출내역을 순번, 적요, 수입, 지출, 잔액으로 화면에 출력한다.
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class AccountMain {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void Register(User user) throws IOException{
+
+        System.out.println("----- 회원가입 -----");
+        System.out.print("1. 이름:");
+        String InputName = sc.nextLine();
+        System.out.println();
+        System.out.print("2. 비밀번호:");
+        String InputPassword = sc.nextLine();
+        boolean UserValidCheck = user.checkName(InputName) && user.checkName(InputPassword);
+
+        if(UserValidCheck){
+            System.out.println("\n▶ 이미 존재하는 계정입니다.");
+        } else{
+            user.setName(InputName);
+            user.setPassword(InputPassword);
+            System.out.println("\n▶ 회원가입 완료");
+        }
+
+    }
+    public static void Login(User user) throws IOException{
+
+        System.out.println("----- 로그인 -----");
+        System.out.print("1. 이름:");
+        String InputName = sc.nextLine();
+        System.out.println();
+        System.out.print("2. 비밀번호:");
+        String InputPassword = sc.nextLine();
+        boolean UserValidCheck = user.checkName(InputName) && user.checkName(InputPassword);
+
+        if(UserValidCheck){
+            System.out.println("\n▶ 로그인 완료");
+            AccountMenu(user); // 메인메뉴
+        } else{
+            System.out.println("\n▶ 존재하지 않는 계정");
+        }
+
+    }
+    public static void InsertAccount(AccountData UserData) {
+        System.out.println();
+        System.out.println("----- 가계부 등록 -----");
+        System.out.print("★ 날짜:");
+        UserData.addDATE(sc.nextLine());
+        System.out.println();
+        System.out.print("☆ 적요:");
+        UserData.addCONTENTS(sc.nextLine());
+        System.out.println();
+        System.out.print("★ 수입:");
+        UserData.addINCOME(sc.nextInt());
+        System.out.println();
+        System.out.print("☆ 지출:");
+        UserData.addSPENDING(sc.nextInt());
+        System.out.println();
+    }
+
+    public static void AccountMenu(User user) {
+        AccountData UserData = new AccountData();
+
+        while(true) {
+            System.out.println("----- 메뉴 -----");
+            System.out.println("1. 삽입");
+            System.out.println("2. 삭제");
+            System.out.println("3. 수정");
+            System.out.println("4. 출력");
+            Scanner sc = new Scanner(System.in);
+
+            int MenuInputKey = sc.nextInt();
+
+            switch (MenuInputKey) {
+                case 1:
+                    InsertAccount(UserData);
+                    break;
+                case 2:
+                    UserData.accountLogPrint();
+                    System.out.println("♣ 삭제를 원하는 번호를 입력하세요.");
+                    UserData.deleteAccountLog(sc.nextInt());
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    UserData.accountLogPrint();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+
+        User user = new User();
+
+        System.out.println("가계부");
+        System.out.println("1. 계정생성");
+        System.out.println("2. 로그인");
+        Scanner sc = new Scanner(System.in);
+
+        int MenuInputKey = sc.nextInt();
+
+
+        switch (MenuInputKey){
+            case 1:
+                Register(user);
+            case 2:
+                Login(user);
+                break;
+            default:
+                break;
+        }
+
+        sc.close();
+
+    }
 }
