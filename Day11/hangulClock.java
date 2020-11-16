ca8a61e7eb7ea257f87a1927dd5e5b374b07ff2e
noel <68936@naver.com>
2020-11-16 16:42:07 +0900

    시간,분에 해당하는 한글 요소 리스트로 선언 및 한글 시계 베이스 배열 구축

--- Day11/hangulClock.java
+++ Day11/hangulClock.java
@@ -1,6 +1,8 @@
 package com.example.Day11;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class hangulClock {
 
@@ -8,57 +10,84 @@ public class hangulClock {
     public static void main(String[] args) {
 
         final int HourMax = 12;
-        SimpleDateFormat timeFormat = new SimpleDateFormat ( "HH:mm");
-        String[] strTime = timeFormat.format (System.currentTimeMillis()).split(":");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String[] strTime = timeFormat.format(System.currentTimeMillis()).split(":");
 
-        String[][] hour = {
-                {"한","두","세","네","다","섯"},
-                {"여","섯","일","곱","여","덟"},
-                {"아","홉","열","한","두","시"},
-        };
-        String[][] minute = {
-                {"자","이","삼","사","오","십"},
-                {"정","일","이","삼","사","육"},
-                {"오","오","칠","팔","구","분"},
-        };
+        List<String> hour = new ArrayList<>();
+        List<String> tens_minute = new ArrayList<>();
+        List<String> units_minute = new ArrayList<>();
+
+        hour.add("시");
+        hour.add("한");
+        hour.add("두");
+        hour.add("세");
+        hour.add("네");
+        hour.add("다 섯");
+        hour.add("여 섯");
+        hour.add("일 곱");
+        hour.add("여 덟");
+        hour.add("아 홉");
+        hour.add("열");
+        hour.add("열 한");
+        hour.add("열 두");
+
+
+        tens_minute.add(".");
+        tens_minute.add("십");
+        tens_minute.add("이");
+        tens_minute.add("삼");
+        tens_minute.add("사");
+        tens_minute.add("오");
+
+
+
+        units_minute.add("분");
+        units_minute.add("일");
+        units_minute.add("이");
+        units_minute.add("삼");
+        units_minute.add("사");
+        units_minute.add("오");
+        units_minute.add("육");
+        units_minute.add("칠");
+        units_minute.add("팔");
+        units_minute.add("구");
 
 
         int nHour = Integer.parseInt(strTime[0]);
+        int nMinute = Integer.parseInt(strTime[1]);
+        int nTenMinute = nMinute/10;
+        int nUnitMinute = nMinute%10;
 
-        if(nHour>12) {
-            strTime[0] = Integer.toString(nHour - HourMax);
-            nHour = Integer.parseInt(strTime[0]);
+        if(nHour>HourMax) {
+            nHour = nHour -HourMax;
+            strTime[0] = Integer.toString(nHour);
         }
-        System.out.println(strTime[0]);
 
-        if (nHour < 5 ) {
-            System.out.print(hour[0][nHour-1]+" ");
-            System.out.println(hour[2][5]+" ");
-        }
-        else if (nHour < 10) {
-            System.out.print(hour[0][nHour-1]+" ");
-            System.out.println(hour[2][5]+" ");
-        }
+
+        System.out.println(strTime[1]);
+        System.out.println(hour.get(nHour));
+        System.out.println(tens_minute.get(nTenMinute));
+        System.out.println(units_minute.get(nUnitMinute));
+
+        String[][] print = {
+                {"한", "두", "세", "네", "다", "섯"},
+                {"여", "섯", "일", "곱", "여", "덟"},
+                {"아", "홉", "열", "한", "두", "시"},
+
+                {"자", "이", "삼", "사", "오", "십"},
+                {"정", "일", "이", "삼", "사", "육"},
+                {"오", "오", "칠", "팔", "구", "분"},
+        };
+
 
         System.out.println();
 
-        for (int i=0;i<hour.length;i++) {
-            for(int j=0;j<hour[i].length;j++) {
+        for(int y=0;y< print.length;y++) {
+            for(int x=0;x<print[0].length;x++){
 
-                System.out.print(hour[i][j] + " ");
+                System.out.print(String.format("%3s\t",print[y][x]));
             }
             System.out.println();
         }
-        for (int i=0;i<minute.length;i++) {
-            for(int j=0;j<minute[i].length;j++) {
-
-                System.out.print(minute[i][j] + " ");
-            }
-            System.out.println();
-        }
-
     }
-
-
-
 }
