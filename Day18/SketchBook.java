2342cee9ab38ef8f2151d5cf4c3117f5cc94bb28
noel <68936@naver.com>
2020-11-26 23:58:39 +0900

    그림판 시작

---
+++ Day18/SketchBook.java
@@ -0,0 +1,12 @@
+package com.example.Day18;
+
+public class SketchBook{
+
+    public static void main(String[] args) {
+
+        paintFrame frame = new paintFrame();
+
+
+    }
+
+}
---
+++ Day18/paintFrame.java
@@ -0,0 +1,8 @@
+package com.example.Day18;
+
+import javax.swing.*;
+
+public class paintFrame extends JFrame {
+
+
+}
